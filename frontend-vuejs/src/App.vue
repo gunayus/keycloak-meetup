23a6f97b28bba46152b3fcf5b99e0a76284a5871
mehmetalikarasurmeli <Mali1994>
2022-02-27 12:05:15 +0300

    Keycloak Setup & Client Configuration

---
+++ frontend-vuejs/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h3>{{'username:' + username}}</h3>
+  <button @click="logout"> Logout </button>
+  <h1>Keycloak Token</h1>
+  <json-viewer :value="keycloakJsonData" copyable boxed sort />
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'App',
+  computed:{
+    ...mapGetters({
+      username: 'username',
+      keycloakJsonData: 'keycloakJsonData'
+    })
+  },
+  methods: {
+    logout(){
+      this.emitter.emit('keycloak-logout')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
